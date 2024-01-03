74505460e8067a8cc9de2006b8431a475ad0fe33
Nimaj Dupanović <93327722+ndupanovi20@users.noreply.github.com>
2024-01-03 15:29:39 +0100

    Character movement
    need to work on it not good, only for testing right now

--- Assets/Movment_main_character.cs
+++ Assets/Movment_main_character.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movment_main_character : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Kretanje levo i desno
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        // Skakanje
+        if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
